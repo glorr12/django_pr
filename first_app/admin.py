8e420f38b2577fda58122d46685f0731efd644f0
glorr12 <igor.fishkov1991@gmail.com>
2026-07-16 00:00:08 +0100

    added task,subtask,category for admin

--- first_app/admin.py
+++ first_app/admin.py
@@ -1,8 +1,28 @@
 from django.contrib import admin
 from .models import Book, Category, SubTask, Task
 
-admin.site.register(Book)
-admin.site.register(Category)
-admin.site.register(SubTask)
-admin.site.register(Task)
+# admin.site.register(Book)
+# admin.site.register(Category)
+# admin.site.register(SubTask)
+# admin.site.register(Task)
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ( 'title', 'status','created_at','deadline')
+    list_filter = ('status',)
+    search_fields = ('title','description',)
+    ordering = ('-created_at',)
+
+
+@admin.register(SubTask)
+class SubTaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'task', 'status','created_at','deadline')
+    list_filter = ('status',)
+    search_fields = ('title','description',)
+    ordering = ('-created_at',)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ('name',)
 
