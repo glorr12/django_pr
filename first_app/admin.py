1e890e45e7e3c34aaf6d65c3ff03b51be3e6ca0e
glorr12 <igor.fishkov1991@gmail.com>
2026-07-24 13:15:56 +0100

    hw#11

--- first_app/admin.py
+++ first_app/admin.py
@@ -1,17 +1,28 @@
 from django.contrib import admin
-from .models import Book, Category, SubTask, Task
+from .models import Book, Category, SubTask, Task, Status
 
 # admin.site.register(Book)
 # admin.site.register(Category)
 # admin.site.register(SubTask)
 # admin.site.register(Task)
+class SubTaskInline(admin.TabularInline):
+    model = SubTask
+    extra = 1
+    fields = ('title','description','status','deadline',)
+
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ( 'title', 'status','created_at','deadline')
+    inlines = (SubTaskInline,)
+    list_display = ( 'short_title', 'status','created_at','deadline')
     list_filter = ('status',)
     search_fields = ('title','description',)
     ordering = ('-created_at',)
+
+    def short_title(self,obj):
+        if len(obj.title) > 10:
+            return f'{obj.title[:10]}...'
+        return obj.title
 
 
 @admin.register(SubTask)
@@ -20,6 +31,13 @@ class SubTaskAdmin(admin.ModelAdmin):
     list_filter = ('status',)
     search_fields = ('title','description',)
     ordering = ('-created_at',)
+    actions = ['mark_done']
+
+
+@admin.action(description='Mark task as Done')
+def mark_done(self, request, queryset):
+    queryset.update(status=Status.DONE)
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
--- first_app/models.py
+++ first_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from icalendar import STATUS
+
 
 
 class Book(models.Model):
